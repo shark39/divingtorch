7825ac42b94fc8451b07e23c4b109fbdb400bfa0
Pascal Crenzin <pascalcrenzin@aol.de>
2015-10-17 16:11:35 -0700

    first version with rgb faders

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name divingtorchApp
+ * @description
+ * # divingtorchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('divingtorchApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngAria',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name divingtorchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the divingtorchApp
+ */
+angular.module('divingtorchApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+  $scope.color = {
+    red: Math.floor(Math.random() * 255),
+    green: Math.floor(Math.random() * 255),
+    blue: Math.floor(Math.random() * 255)
+  };
+  
+  
+  }]);
+
+
+
