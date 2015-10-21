a4b03b158c61c93f0f3c7e8ac1a2c8c8f704f8d7
Michael Janke <michael.jank@student.hpi.uni-potsdam.de>
2015-10-21 00:04:34 -0700

    own colorpicker

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,28 @@
  * Controller of the divingtorchApp
  */
 angular.module('divingtorchApp')
-  .controller('MainCtrl', ['$scope', '$model', 'app', '$http', function ($scope, $model, app, $http) {
-  $scope.color = {
-    red: Math.floor(Math.random() * 255),
-    green: Math.floor(Math.random() * 255),
-    blue: Math.floor(Math.random() * 255)
-  };
-
+  .controller('MainCtrl', ['$scope', '$model', 'app', '$http', '$timeout', function ($scope, $model, app, $http, $timeout) {
+  $scope.color = '#000000';
+  $scope.colorList = ["#5484ED", "#A4BDFC", "#46D6DB", "#7AE7BF",
+    "#51B749", "#FBD75B", "#FFB878", "#FF887C", "#DC2127",
+    "#DBADFF", "#E1E1E1"];
+  $scope.applyType = "move";
   $scope.getColor = function() {
-  	return 'rgb(' + $scope.color.red + ',' + $scope.color.green + ',' + $scope.color.blue + ')';  
+  	return $scope.color;  
   }
   $scope.style = {'background-color' : '#FFFFFF'};
-  $scope.applyColor = function() {
-  	var color = $scope.color.blue + ($scope.color.green * 256) + ($scope.color.red * 256 * 256);
+
+  $scope.applyStop = false;
+  $scope.applyColor = function(color, type) {
+    if($scope.applyType == type && !$scope.applyStop) {
+      $scope.send(color);
+      $scope.applyStop = true;
+      $scope.limiter = $timeout(function() {$scope.applyStop = false}, 100);
+    }
+  };
+
+  $scope.send = function() {
+  	var color = parseInt($scope.color, 16);
   	$scope.style = {'background-color' : $scope.getColor()};
   	$http.post(app.url + "/pixel", {rgb: color, areas: $model.applyTo}, {headers: {  'Content-Type': 'application/json'  }});
   }
---
+++ app/scripts/directives/colorpicker.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name sideNavApp.directive:colorPicker
+ * @description
+ * # colorPicker
+ */
+angular.module('divingtorchApp')
+  .directive('colorPicker', function () {
+    return {
+      template: '<div layout="row" layout-fill style="height:100px; min-height:20px"><div flex ng-repeat="colorEl in colorList" style="border: {{color == colorEl}}px solid #000;\
+      				background-color: {{colorEl}}" ng-mousemove="selectColor(colorEl,\'move\')" ng-click="selectColor(colorEl,\'click\')"></div>\
+      			</div>',
+      restrict: 'E',
+      scope: {
+      	colorList: "=",
+      	color: "=",
+      	onChange: "="
+      },
+      link: function postLink(scope, element, attrs) {
+      },
+      controller: function($scope, $element, $timeout) {
+        $scope.selectColor = function(color, type) {
+        	$scope.color = color;
+        	if($scope.onChange) $scope.onChange(color, type);};
+      	}
+    };
+  });
